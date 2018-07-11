6c5539c11971d68bfc00a1edcbba5442e25ca55a
novakvova <novakvova@gmail.com>
2018-07-11 15:35:27 +0300

    ssss

---
+++ src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  postQuestion(question) {
+    this.http.post('http://localhost:45974/api/questions',question).subscribe(res=> {
+      console.log(res);
+    })
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
